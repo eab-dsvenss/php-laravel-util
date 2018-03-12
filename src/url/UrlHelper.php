f5821c3b491ab4150ea7dac1bb440762c2f1c955
Daniel Svensson <daniel@eskeliden.se>
2018-03-12 15:09:10 +0100

    Singleton pattern

--- src/url/UrlHelper.php
+++ src/url/UrlHelper.php
@@ -17,10 +17,19 @@ use Illuminate\Http\Request;
  */
 class UrlHelper
 {
+    private static $instance;
 
-    public function __construct()
+    private function __construct()
     {
         
+    }
+    
+    public static function getInstance() {
+        if (!isset(self::$instance)) {
+            self::$instance = new UrlHelper();
+        }
+        
+        return self::$instance;
     }
 
     /**
